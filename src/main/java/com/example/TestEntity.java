67990fd7f896191608337dd74c76a191c33cda38
seth@vademecum.uk <114396735+vademecumuk@users.noreply.github.com>
2022-12-19 13:26:49 +0000

    Working with aws-cognito for oauth2 user login.

---
+++ src/main/java/com/example/TestEntity.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter@Setter
+@ToString
+public class TestEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    String value;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        TestEntity that = (TestEntity) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
